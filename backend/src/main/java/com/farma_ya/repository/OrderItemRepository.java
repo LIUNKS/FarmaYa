cc6609fba8394cdab3d4b570e8e14392871dc6f2
jgcamiloaga <jcamiloc148@gmail.com>
2025-11-17 22:53:34 -0500

    feat: add OrderItemRepository interface extending JpaRepository

---
+++ backend/src/main/java/com/farma_ya/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.farma_ya.repository;
+
+import com.farma_ya.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
