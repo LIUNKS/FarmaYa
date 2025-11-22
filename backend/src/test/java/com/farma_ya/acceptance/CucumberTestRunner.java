77363b3bb37f54d6245116af599dfc6a781b0b72
jgcamiloaga <jcamiloc148@gmail.com>
2025-11-22 10:49:17 -0500

    feat: Add Cucumber acceptance tests for order management.

--- backend/src/test/java/com/farma_ya/acceptance/CucumberTestRunner.java
+++ backend/src/test/java/com/farma_ya/acceptance/CucumberTestRunner.java
@@ -1,8 +1,11 @@
 package com.farma_ya.acceptance;
 
+import com.farma_ya.FarmaYaApplication;
 import io.cucumber.junit.platform.engine.Cucumber;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @Cucumber
+@SpringBootTest(classes = FarmaYaApplication.class)
 public class CucumberTestRunner {
     // This class serves as a holder for the @Cucumber annotation
     // Cucumber will automatically discover and run all feature files and step
