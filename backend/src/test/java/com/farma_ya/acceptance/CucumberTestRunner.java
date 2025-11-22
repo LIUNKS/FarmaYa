30030ad0bf2a891f9b850ad4047607faa98be453
jgcamiloaga <jcamiloc148@gmail.com>
2025-11-22 16:27:42 -0500

    feat: Refactor CucumberTestRunner to use CucumberContextConfiguration and update SpringBootTest configuration

--- backend/src/test/java/com/farma_ya/acceptance/CucumberTestRunner.java
+++ backend/src/test/java/com/farma_ya/acceptance/CucumberTestRunner.java
@@ -1,11 +1,14 @@
 package com.farma_ya.acceptance;
 
-import com.farma_ya.FarmaYaApplication;
 import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @Cucumber
-@SpringBootTest(classes = FarmaYaApplication.class)
+@CucumberContextConfiguration
+@SpringBootTest
+@ContextConfiguration(classes = CucumberTestConfig.class)
 public class CucumberTestRunner {
     // This class serves as a holder for the @Cucumber annotation
     // Cucumber will automatically discover and run all feature files and step
